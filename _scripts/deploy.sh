5a4fa681e1022c677eb89a2a28439234405d8abe
David Okun <david@okun.io>
2018-07-29 09:13:36 -0700

    Trying new approach

---
+++ _scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+then
+    ncftp -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io"<<EOF
+    rm -rf site/wwwroot
+    mkdir site/wwwroot
+    quit
+EOF
+
+    cd _site || exit
+    ncftpput -R -v -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io" /site/wwwroot .
+fi
