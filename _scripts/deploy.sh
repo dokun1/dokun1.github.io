e93c8cb9e8b9b6a2b05205e1f1f1b032cb1781f6
David Okun <david@okun.io>
2018-12-30 10:55:13 -0600

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-    cd _site || exit
-    ncftpput -R -v -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io" ./david/ .
-
-fi
+cd _site || exit
+ncftpput -R -v -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io" ./david/ .
