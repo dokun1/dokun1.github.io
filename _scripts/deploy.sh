07bde334351ecf51b6296cb49552d5d2b3c4e6cd
David Okun <david@okun.io>
2018-12-30 10:49:02 -0600

    Updated deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-    # ncftp -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io"<<EOF
-    # rm -rf site/wwwroot
-    # mkdir site/wwwroot
-    # quit
-EOF
-    
     cd _site || exit
-    ncftpput -R -v -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io" ./ .
     ncftpput -R -v -u "$FTPUSERNAME" -p "$FTPPASSWORD" "ftp.okun.io" ./david/ .
 
 fi
